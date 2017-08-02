55cecf56d5f442d096b5a8a0ef3e14b81f4cb04b
Rohit Singh Adhikari <rohadhik@gmail.com>
2017-08-02 12:04:22 +0530

    Create web_blocker.py

---
+++ web_blocker.py
@@ -0,0 +1,32 @@
+import time
+from datetime import datetime as dt
+hosts_temp = "C:\python3\learn\settingup\web_blocker\hosts"
+hosts_path = "c:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+
+website_list = ["www.facebook.com","facebook.com","www.linkedin.com/feed/","linkedin.com/feed/"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,13) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
+        print ("Working hours....")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    #print ("hello")
+                    content1 = file.write(redirect+"    "+ website + "\n")
+
+    else:
+        print ("Fun Hours....")
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+            print ("No problem.....")
+
+    time.sleep(5)
